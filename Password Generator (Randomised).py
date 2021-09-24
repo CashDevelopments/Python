af68c8ba0cd0477a46f31662c79fee03c8d6e394
Skidded <91278886+Skiddedd@users.noreply.github.com>
2021-09-24 07:52:26 +0100

    Randomised Password Generator
    This Random Password Generator can generate passwords up to 76 or so characters long, which include no repeated characters.

---
+++ Password Generator (Randomised).py
@@ -0,0 +1,13 @@
+##############################################################################################
+#                          Password Generator made by Skidded#8658                           #
+##############################################################################################
+
+import string
+from random import sample
+
+characters=string.ascii_letters+string.digits+string.punctuation
+
+length=input("How many characters long would you like your password to be?\n > ")
+password="".join(sample(characters,int(length)))
+
+print(password)
